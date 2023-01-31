933b9b078c6cb1f0b0ed2e45223764555d7d11f7
Lucas Rezende <rezendelc94@gmail.com>
2023-01-31 20:37:03 -0300

    added new components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,38 @@
+import React, { useState } from 'react'
+
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
-        <Header />
+      <Header />
+      
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={ <ListEmpty message="Que tal adicionar um novo grupo?"/>}
+      />
+
+      
     </Container>
   );
 }
