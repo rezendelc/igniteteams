1958535c2223bbae558f828c1fc07405272fb4e3
Lucas Rezende <rezendelc94@gmail.com>
2023-01-30 21:28:10 -0300

    added phosphor icons and header component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import theme from 'src/theme';
+import theme from './src/theme';
 import { Groups } from '@screens/Groups'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -10,6 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
