0a55ce44fae3c5c569c1a5d7e04ce2ca9b0f5e8f
Lucas Rezende <rezendelc94@gmail.com>
2023-02-07 20:48:45 -0300

    added input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
@@ -16,8 +17,11 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
+        <Input />
+
         <Button
           title="Criar"
+          style={{marginTop: 20}}
         />
 
       </Content>
