a3e9c7659857e255fa189fe41336d5f9b97e0a78
Lucas Rezende <rezendelc94@gmail.com>
2023-02-13 21:42:09 -0300

    set filter to player screen and number of players

--- src/components/Players/index.tsx
+++ src/components/Players/index.tsx
@@ -1,30 +1,58 @@
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Input } from '@components/Input';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { Header } from '@components/Header';
+import { Filter } from '@components/Filter';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
-import { Input } from '@components/Input';
 
 export function Players() {
-    return (
-      <Container>
-        <Header showBackButton />
 
-        <Highlight
-          title="Nome da turma"
-          subtitle="adicione a galera e separe os times"
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([])
+
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input
+          placeholder='Nome da pessoa'
+          autoCorrect={false}
+          />
+
+        <ButtonIcon
+          icon="add"
+          type="PRIMARY"
+        />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={team === item}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
         />
 
-        <Form>
-          <Input
-            placeholder='Nome da pessoa'
-            autoCorrect={false}
-            />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
 
-          <ButtonIcon
-            icon="add"
-            type="PRIMARY"
-          />
-        </Form>
-      </Container>
-    );
+    </Container>
+  );
 }
